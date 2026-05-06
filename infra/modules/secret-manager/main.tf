d3abdba90380cae75c0223ba8c498ab0b2b77f9e
Kimmo Heikkilä <kimmo.heikkila@digia.com>
2026-05-06 13:20:11 +0300

    Update main.tf

--- infra/modules/secret-manager/main.tf
+++ infra/modules/secret-manager/main.tf
@@ -23,7 +23,7 @@ resource "google_secret_manager_secret" "this" {
   replication {
     user_managed {
       replicas {
-        location = var.region
+        location = var.gcp_region
       }
     }
   }
