5587d8d1808667f0c0ee1f314b0f67954ac7e50c
Kimmo Heikkilä <kimmo.heikkila@digia.com>
2026-05-06 13:24:36 +0300

    Update variables.tf

--- infra/modules/secret-manager/variables.tf
+++ infra/modules/secret-manager/variables.tf
@@ -17,11 +17,6 @@ variable "gcp_project_id" {
   description = "The GCP Project ID where the secrets will be created."
 }
 
-variable "gcp_region" {
-  type        = string
-  description = "The GCP region for the resources."
-}
-
 variable "secret_names" {
   type        = list(string)
   description = "A list of secret IDs to create (e.g., [\"FIREBASE_API_KEY\", \"GOOGLE_CLIENT_ID\"])."
